36264423487913bef7374e48ccfdb15ff5145d9b
pickledbroccoli <163914034+pickledbroccoli@users.noreply.github.com>
2024-04-16 14:14:02 +0200

    add data for quest and facts. Fix medium size bug

--- messages.js
+++ messages.js
@@ -7,9 +7,9 @@ function randomSelector(arr) {
 
 const locations = ['an ominous crossroad', 'an ancient ruin of the Elder Ones', 'the gates of DustBottom City', 'a broken cart', 'a nice oak tree', 'an empty well', 'the cemetery gates', 'Rick\'s American Bar', 'a quiet village', 'the mouth of a dark cave', 'a creepy old windmill', 'a peaceful lake', 'the korean DMZ', 'the End of the World - as we know it', 'the community pool'];
 const datetimes = ['nighttime', 'high noon', 'just a few minutes after breakfast time', 'two minutes to midnight', '16:20', 'dinner time', 'party time', 'HammerTime!', 'late afternoon', 'early night after a long day', 'dusk', 'the dawn of the new day'];
-const quests = [];
+const quests = ['retrieve money from the ATM', 'cook lunch for the boys', 'save Princess Apricot', 'take over the local KFC', 'fetch water form the well', 'drink a coffee', 'deliver an important message to the king', 'catch the leader of the yakuza'];
 const legendaries = []; //later
-const facts = [];
+const facts = ['they are afraid of wooden spoons', 'they must be cleansed with distilled water', 'they can\'t pronounce the letter "f"', 'they can toggle night-vision', 'they are immune to the common cold', 'they can eat a paella bowl as a whole', 'they not allowed to use stairs', 'they aren\'t real', 'they can see in the dark', 'they can smell color'];
 const creatureNames = ['gnome', 'hobgoblin', 'hydra', 'miniature dragon', 'almost-wyvern', 'succubus', 'troll', 'halfling', 'elven ranger', 'undead wizard', 'lych', 'ghoul', 'vampire', 'three-dwarves-in-a-trench-coat', 'wild sheep', 'mutant', 'zombie', 'flesh-eating amoeba', 'gelatinous tetrahedron', 'ork bandit'];
 const adjectives = ['hairy', 'exceptionally tall', 'blue-feathered', 'pesky', 'hungry', 'fat', 'swollen', 'thicc', 'green', 'yellow-tainted', 'sad', 'jumping', 'misunderstood', 'tattooed', 'skin-and-bones', 'sleepy', 'angry', 'raging', 'sitting', 'letivating', 'fruitfly-haloed', 'drunk', 'red'];
 const sizes = ['small', 'medium', 'large', 'enormous'];
@@ -45,21 +45,21 @@ function randomTemplateFiller(){
             break;
         case 'medium':
             multiple = 's';
-            howMany = Math.ceil(Math.random() * 3);
+            howMany = Math.ceil(Math.random() * 3) + 1;
             break;
         case 'large':
             multiple = 's';
             howMany = Math.ceil(Math.random() * 5) + 2;
             break;
-        case 'enourmous':
+        case 'enormous':
             multiple = 's';
             howMany = Math.ceil(Math.random() * 10) + 9;
+            break;
     }
 
     return `You are on your way to ${quest}. It is ${time}, and you arrive at ${location}.
     Oh! Suddenly a${adjective} ${creature} appears! It has ${howMany} ${noun}${multiple} as its ${body}${multiple}.
-    You've heard of these before - ${fact}.
-    `;
+    You've heard of these before - ${fact}.`;
 }
 
 
