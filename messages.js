467036281163cefde4a1e772a488da1105625782
pickledbroccoli <163914034+pickledbroccoli@users.noreply.github.com>
2024-04-16 13:51:26 +0200

    add data for other text arrays, update readme file

--- messages.js
+++ messages.js
@@ -6,15 +6,15 @@ function randomSelector(arr) {
 }
 
 const locations = ['an ominous crossroad', 'an ancient ruin of the Elder Ones', 'the gates of DustBottom City', 'a broken cart', 'a nice oak tree', 'an empty well', 'the cemetery gates', 'Rick\'s American Bar', 'a quiet village', 'the mouth of a dark cave', 'a creepy old windmill', 'a peaceful lake', 'the korean DMZ', 'the End of the World - as we know it', 'the community pool'];
-const datetimes = [];
+const datetimes = ['nighttime', 'high noon', 'just a few minutes after breakfast time', 'two minutes to midnight', '16:20', 'dinner time', 'party time', 'HammerTime!', 'late afternoon', 'early night after a long day', 'dusk', 'the dawn of the new day'];
 const quests = [];
-const legendaries = [];
+const legendaries = []; //later
 const facts = [];
-const creatureNames = [];
-const adjectives = [];
+const creatureNames = ['gnome', 'hobgoblin', 'hydra', 'miniature dragon', 'almost-wyvern', 'succubus', 'troll', 'halfling', 'elven ranger', 'undead wizard', 'lych', 'ghoul', 'vampire', 'three-dwarves-in-a-trench-coat', 'wild sheep', 'mutant', 'zombie', 'flesh-eating amoeba', 'gelatinous tetrahedron', 'ork bandit'];
+const adjectives = ['hairy', 'exceptionally tall', 'blue-feathered', 'pesky', 'hungry', 'fat', 'swollen', 'thicc', 'green', 'yellow-tainted', 'sad', 'jumping', 'misunderstood', 'tattooed', 'skin-and-bones', 'sleepy', 'angry', 'raging', 'sitting', 'letivating', 'fruitfly-haloed', 'drunk', 'red'];
 const sizes = ['small', 'medium', 'large', 'enormous'];
-const bodyParts = [];
-const nouns = [];
+const bodyParts = ['big toe', 'knee', 'mouth', 'ear', 'nose', 'chesthair', 'jaw', 'eyebrow', 'elbow', 'hand', 'leg', 'nipple', 'lip', 'arm', 'neck'];
+const nouns = ['spoon', 'juniper tree', 'sword', 'microchip', 'red chief apple', 'chicken feather', 'worm', 'coffee break', 'rope', 'whisk', 'pen', 'mug', 'knitted sweater', 'jumper cable'];
 let multiple = '';
 let howMany = '';
 
