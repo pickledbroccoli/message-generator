1c7a226da8519a0e3dc42f90e4695da22b61bc9a
pickledbroccoli <163914034+pickledbroccoli@users.noreply.github.com>
2024-04-16 13:17:39 +0200

    create basic template with randomizer

---
+++ messages.js
@@ -0,0 +1,66 @@
+// random selector for a dictionary
+function randomSelector(arr) {
+    let index = Math.floor(Math.random() * arr.length);
+
+    return arr[index];
+}
+
+const locations = [];
+const datetimes = [];
+const quests = [];
+const legendaries = [];
+const facts = [];
+const creatureNames = [];
+const adjectives = [];
+const sizes = ['small', 'medium', 'large', 'enormous'];
+const bodyParts = [];
+const nouns = [];
+let multiple = '';
+let howMany = '';
+
+function randomTemplateFiller(){
+
+    let quest = randomSelector(quests);
+    let time = randomSelector(datetimes);
+    let location = randomSelector(locations);
+    let adjective = randomSelector(adjectives);
+    
+    // a(n) handling
+    if (/^[aeiou]/.test(adjective)){
+        adjective = 'n ' + adjective;
+    } else {
+        adjective = ' ' + adjective;
+    }
+
+    let creature = randomSelector(creatureNames);
+    let noun = randomSelector(nouns);
+    let body = randomSelector(bodyParts);
+    let fact = randomSelector(facts);
+    let size = randomSelector(sizes);
+
+    // sizewise...
+    switch (size) {
+        case 'small':
+            howMany = 'a';
+            break;
+        case 'medium':
+            multiple = 's';
+            howMany = Math.ceil(Math.random() * 3);
+            break;
+        case 'large':
+            multiple = 's';
+            howMany = Math.ceil(Math.random() * 5) + 2;
+            break;
+        case 'enourmous':
+            multiple = 's';
+            howMany = Math.ceil(Math.random() * 10) + 9;
+    }
+
+    return `You are on your way to ${quest}. It is ${time}, and you arrive at ${location}.
+    Oh! Suddenly a${adjective} ${creature} appears! It has ${howMany} ${noun}${multiple} as its ${body}${multiple}.
+    You've heard of these before - ${fact}.
+    `;
+}
+
+
+console.log(randomTemplateFiller());
